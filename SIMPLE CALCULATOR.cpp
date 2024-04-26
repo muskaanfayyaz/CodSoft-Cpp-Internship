9e728c24bb5ff6ed3189b1b21b9fd4acb0f763a3
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2024-04-26 23:43:28 +0500

    SIMPLE CALCULATOR

--- SIMPLE CALCULATOR.cpp
+++ SIMPLE CALCULATOR.cpp
@@ -12,27 +12,33 @@ int main()
 	switch (op)
 	{
 		case '+' :{ //if user enter + add both num
-			x=num1+num2;
+			x = num1 + num2;
 			cout<<"result: "<<x;
 			break;
 		}
 		case '-' :{ //if user enter - subtract both num
-			x=num1-num2;
+			x = num1 - num2;
 			cout<<"result: "<<x;
 			break;
 		}
 		case '*' :{ //if user enter * multiply both num
-			x=num1*num2;
+			x = num1 * num2;
 			cout<<"result: "<<x;
 			break;
 		}
 		case '/' :{ //if user enter / divide both num
-			x=num1/num2;
+		    if(num2 == 0 )
+		    {
+		        cout<< "ERROR! division by zero is not possible. "; 
+		    }
+	 	     else 
+		    { 	
+			x = num1 / num2;
 			cout<<"result: "<<x;
 			break;
-		}
+		     }
 		case '%' :{ //if user enter % it calculate the remainder 
-			x=num1%num2;
+			x = num1 % num2;
 			cout<<"result: "<<x;
 			break;
 		}
